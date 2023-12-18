f5a50c02abb78f83dc3766a890eb2729a43fdaff
Stephen Muñez <sjtmunez@gmail.com>
2023-12-18 09:47:41 +0800

    setup: fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
@@ -6,6 +7,9 @@ export default {
       'pure-black': '#000000',
       'light-grey': '#DFDFDF',
       'pure-white': '#FFFFFF',
+    },
+    fontFamily: {
+      sans: ['"DM Sans"', ...defaultTheme.fontFamily.sans],
     },
   },
   plugins: [],

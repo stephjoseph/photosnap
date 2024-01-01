68e7676d79a6a772af5e2035e3cb9c4ea1e494f1
Stephen Muñez <sjtmunez@gmail.com>
2024-01-01 14:23:14 +0800

    feat: home showcase mobile

---
+++ src/lib/home/home-showcase.svelte
@@ -0,0 +1,54 @@
+<script>
+  import { onMount, onDestroy } from 'svelte';
+  import arrow from '../../assets/shared/desktop/arrow.svg';
+
+  //   export let imagePosition;
+  export let showcase;
+
+  let windowWidth = 0;
+
+  onMount(() => {
+    windowWidth = window.innerWidth;
+
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  });
+
+  function handleResize() {
+    windowWidth = window.innerWidth;
+  }
+
+  $: heroImageUrl =
+    windowWidth < 768
+      ? showcase.image.mobile
+      : windowWidth >= 768 && windowWidth < 1280
+        ? showcase.image.tablet
+        : showcase.image.desktop;
+</script>
+
+<div class="flex w-full flex-col">
+  <div class="w-full">
+    <img class="h-full w-full" src={heroImageUrl} alt="hero" />
+  </div>
+  <div class="flex w-full flex-col gap-6 bg-pure-white py-[4.5rem] pl-8 pr-6">
+    <div class="flex w-full flex-col gap-4">
+      <h1
+        class="text-[2rem] font-bold uppercase leading-10 tracking-[3.33px] text-pure-black"
+      >
+        {showcase.title}
+      </h1>
+      <p class="text-[0.938rem] font-normal tracking-normal text-pure-black/60">
+        {showcase.description}
+      </p>
+    </div>
+    <a
+      class="flex w-fit items-center gap-[1.125rem] text-[0.75rem] font-bold uppercase leading-4 tracking-[2px] text-pure-black"
+      href="/"
+    >
+      <span>{showcase.cta}</span>
+      <img src={arrow} alt="arrow icon" />
+    </a>
+  </div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,44 @@
 <script>
   import HomeHero from '$lib/home/home-hero.svelte';
+  import HomeShowcase from '../lib/home/home-showcase.svelte';
+  import showcaseImg1Mobile from '../assets/home/mobile/beautiful-stories.jpg';
+  import showcaseImg1Tablet from '../assets/home/tablet/beautiful-stories.jpg';
+  import showcaseImg1Desktop from '../assets/home/desktop/beautiful-stories.jpg';
+  import showcaseImg2Mobile from '../assets/home/mobile/designed-for-everyone.jpg';
+  import showcaseImg2Tablet from '../assets/home/tablet/designed-for-everyone.jpg';
+  import showcaseImg2Desktop from '../assets/home/desktop/designed-for-everyone.jpg';
+
+  let showcase1 = {
+    title: 'Beautiful stories everytime',
+    description:
+      'We provide design templates to ensure your stories look terrific. Easily add photos, text, embed maps and media from other networks. Then share your story with everyone.',
+    cta: 'View the stories',
+    image: {
+      mobile: showcaseImg1Mobile,
+      tablet: showcaseImg1Tablet,
+      desktop: showcaseImg1Desktop,
+      position: 'right',
+    },
+  };
+
+  let showcase2 = {
+    title: 'Designed for everyone',
+    description:
+      'Photosnap can help you create stories that resonate with your audience.  Our tool is designed for photographers of all levels, brands, businesses you name it.',
+    cta: 'View the stories',
+    image: {
+      mobile: showcaseImg2Mobile,
+      tablet: showcaseImg2Tablet,
+      desktop: showcaseImg2Desktop,
+      position: 'right',
+    },
+  };
 </script>
 
 <main>
   <HomeHero />
+  <HomeShowcase showcase={showcase1} />
+  <HomeShowcase showcase={showcase2} />
 </main>
 
 <style>

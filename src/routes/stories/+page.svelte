2c64f271e2ee57f6861587690501d15ea9605309
Stephen Muñez <sjtmunez@gmail.com>
2024-01-04 11:24:10 +0800

    feat: stories mobile

--- src/routes/stories/+page.svelte
+++ src/routes/stories/+page.svelte
@@ -1,7 +1,9 @@
 <script>
   import StoriesHero from '../../lib/stories/stories-hero.svelte';
+  import StoriesStories from '../../lib/stories/stories-stories.svelte';
 </script>
 
 <main>
   <StoriesHero />
+  <StoriesStories />
 </main>

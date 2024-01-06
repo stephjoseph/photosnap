f5c0c86a3473bf75333266270f444044dcc510ac
Stephen Muñez <sjtmunez@gmail.com>
2024-01-07 05:34:38 +0800

    feat: feature features mobile

--- src/routes/features/+page.svelte
+++ src/routes/features/+page.svelte
@@ -1,7 +1,9 @@
 <script>
+  import FeaturesFeatures from '../../lib/features/features-features.svelte';
   import FeaturesHero from '../../lib/features/features-hero.svelte';
 </script>
 
 <main class="mt-[4.5rem]">
   <FeaturesHero />
+  <FeaturesFeatures />
 </main>

e8aee64239f7a940d3713f08235e79d03548c4d9
Stephen Muñez <sjtmunez@gmail.com>
2024-01-08 03:18:01 +0800

    feat: pricing plans mobile

---
+++ src/lib/pricing/pricing-plans.svelte
@@ -0,0 +1,158 @@
+<script>
+  let isYearly;
+  let plans = [
+    {
+      title: 'Basic',
+      description:
+        'Includes basic usage of our platform. Recommended for new and aspiring photographers.',
+      price: 19.0,
+    },
+    {
+      title: 'Pro',
+      description:
+        'More advanced features available. Recommended for photography veterans and professionals.',
+      price: 39.0,
+    },
+    {
+      title: 'Business',
+      description:
+        'Additional features available such as more detailed metrics. Recommended for business owners.',
+      price: 99.0,
+    },
+  ];
+
+  const toggleYearly = () => {
+    isYearly = !isYearly;
+  };
+</script>
+
+<section class="relative flex w-full flex-col items-center gap-10 px-7 py-16">
+  <h2 class="invisible absolute -left-[9999px]">Pricing Plans</h2>
+  <div class="pricing__switch">
+    <span
+      class:font-bold={!isYearly}
+      class="w-[4.5rem] text-[1.125rem] leading-[1.563rem] tracking-normal text-pure-black"
+      >Monthly</span
+    >
+    <label>
+      <input type="checkbox" bind:checked={isYearly} on:click={toggleYearly} />
+      <span />
+    </label>
+    <span
+      class:font-bold={isYearly}
+      class="w-14 text-[1.125rem] leading-[1.563rem] tracking-normal text-pure-black"
+      >Yearly</span
+    >
+  </div>
+  <div class="flex w-full flex-col gap-6">
+    {#each plans as plan, i (plan.title)}
+      <div
+        class="relative flex w-full flex-col items-center gap-10 px-5 pb-10 pt-14 {i ===
+        1
+          ? 'bg-pure-black'
+          : 'bg-[#F5F5F5]'}"
+      >
+        {#if i === 1}
+          <div
+            class="absolute left-0 top-0 block h-[0.375rem] w-full bg-gradient-to-tr from-[#FFC593] from-0% via-[#BC7198] via-[43.29%] to-[#5A77FF] to-[83.33%]"
+          ></div>
+        {/if}
+        <div class="flex flex-col items-center gap-[1.125rem]">
+          <h3
+            class="text-center text-[1.5rem] font-bold leading-[1.563rem] tracking-normal {i ===
+            1
+              ? 'text-pure-white'
+              : 'text-pure-black'}"
+          >
+            {plan.title}
+          </h3>
+          <p
+            class="text-center text-[0.938rem] font-normal leading-[1.563rem] tracking-normal {i ===
+            1
+              ? 'text-pure-white/60'
+              : 'text-pure-black/60'}"
+          >
+            {plan.description}
+          </p>
+        </div>
+        <div class="flex flex-col items-center">
+          <span
+            class="text-[2.5rem] font-bold leading-[3rem] tracking-[4.17px] {i ===
+            1
+              ? 'text-pure-white'
+              : 'text-pure-black'}"
+            >${isYearly
+              ? (plan.price * 10).toFixed(2)
+              : plan.price.toFixed(2)}</span
+          >
+          <span
+            class="text-center text-[0.938rem] font-normal leading-[1.563rem] tracking-normal {i ===
+            1
+              ? 'text-pure-white/60'
+              : 'text-pure-black/60'}">per {isYearly ? 'year' : 'month'}</span
+          >
+        </div>
+        <a
+          class="w-[87.81%] py-3 text-center text-[0.75rem] font-bold uppercase leading-4 tracking-[2px] hover:bg-light-grey hover:text-pure-black {i ===
+          1
+            ? 'bg-pure-white text-pure-black'
+            : 'bg-pure-black text-pure-white'}"
+          href="/">Pick plan</a
+        >
+      </div>
+    {/each}
+  </div>
+</section>
+
+<style>
+  .pricing__switch {
+    display: flex;
+    align-items: center;
+    gap: 32px;
+  }
+
+  .pricing__switch label {
+    position: relative;
+    display: inline-block;
+    width: 64px;
+    height: 32px;
+  }
+
+  .pricing__switch label input {
+    opacity: 0;
+    width: 0;
+    height: 0;
+  }
+
+  .pricing__switch label span {
+    position: absolute;
+    cursor: pointer;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: #dfdfdf;
+    -webkit-transition: 0.4s;
+    transition: 0.4s;
+    border-radius: 34px;
+  }
+
+  .pricing__switch label span::before {
+    position: absolute;
+    content: '';
+    height: 24px;
+    width: 24px;
+    left: 4px;
+    bottom: 4px;
+    background-color: #000000;
+    -webkit-transition: 0.4s;
+    transition: 0.4s;
+    border-radius: 50%;
+  }
+
+  .pricing__switch label input:checked + span:before {
+    -webkit-transform: translateX(32px);
+    -ms-transform: translateX(32px);
+    transform: translateX(32px);
+  }
+</style>
--- src/routes/pricing/+page.svelte
+++ src/routes/pricing/+page.svelte
@@ -1,7 +1,9 @@
 <script>
   import PricingHero from '../../lib/pricing/pricing-hero.svelte';
+  import PricingPlans from '../../lib/pricing/pricing-plans.svelte';
 </script>
 
 <main class="mt-[7.5rem]">
   <PricingHero />
+  <PricingPlans />
 </main>

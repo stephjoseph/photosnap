3976dcf16fcecac009e8ec10647296f328a5ff16
Stephen Muñez <sjtmunez@gmail.com>
2024-01-08 03:18:41 +0800

    feat: pricing cta

--- src/routes/pricing/+page.svelte
+++ src/routes/pricing/+page.svelte
@@ -1,9 +1,11 @@
 <script>
   import PricingHero from '../../lib/pricing/pricing-hero.svelte';
   import PricingPlans from '../../lib/pricing/pricing-plans.svelte';
+  import Cta from '../../lib/shared/cta.svelte';
 </script>
 
 <main class="mt-[7.5rem]">
   <PricingHero />
   <PricingPlans />
+  <Cta />
 </main>

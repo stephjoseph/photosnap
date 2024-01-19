e4f51432ac64d8e15e77087b27991f8ab3d13ca6
Stephen Muñez <sjtmunez@gmail.com>
2024-01-19 09:55:39 +0800

    feat: header active states

--- src/lib/shared/header.svelte
+++ src/lib/shared/header.svelte
@@ -12,19 +12,19 @@
     <ul class="flex items-center gap-9">
       <li>
         <a
-          class="text-[0.75rem] font-bold uppercase leading-4 tracking-[2px] text-pure-black"
+          class="text-[0.75rem] font-bold uppercase leading-4 tracking-[2px] text-pure-black transition-colors hover:text-pure-black/30 active:text-pure-black/30"
           href="/stories">Stories</a
         >
       </li>
       <li>
         <a
-          class="text-[0.75rem] font-bold uppercase leading-4 tracking-[2px] text-pure-black"
+          class="text-[0.75rem] font-bold uppercase leading-4 tracking-[2px] text-pure-black transition-colors hover:text-pure-black/30 active:text-pure-black/30"
           href="/features">Features</a
         >
       </li>
       <li>
         <a
-          class="text-[0.75rem] font-bold uppercase leading-4 tracking-[2px] text-pure-black"
+          class="text-[0.75rem] font-bold uppercase leading-4 tracking-[2px] text-pure-black transition-colors hover:text-pure-black/30 active:text-pure-black/30"
           href="/pricing">Pricing</a
         >
       </li>
@@ -32,7 +32,7 @@
   </nav>
   <a
     href="/"
-    class="w-40 bg-pure-black py-[0.75rem] text-center text-[0.75rem] font-bold uppercase leading-4 tracking-[2px] text-pure-white"
+    class="transtion-colors w-40 bg-pure-black py-[0.75rem] text-center text-[0.75rem] font-bold uppercase leading-4 tracking-[2px] text-pure-white hover:bg-light-grey hover:text-pure-black active:bg-light-grey active:text-pure-black"
     >Get an invite</a
   >
 </header>
